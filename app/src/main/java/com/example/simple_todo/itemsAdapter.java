b8239859c41adb17f10bc7c98ad4184cfb5302c1
Tamajong Nchukwi <tjnc@Tamajongs-MacBook-Pro-2.local>
2021-09-10 21:01:31 -0400

    Simple Todo App

---
+++ app/src/main/java/com/example/simple_todo/itemsAdapter.java
@@ -0,0 +1,71 @@
+package com.example.simple_todo;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+//
+public class itemsAdapter extends RecyclerView.Adapter<itemsAdapter.ViewHolder>{
+    public interface OnLongClickListener {
+        void onItemLongClicked(int position);
+    }
+
+    public interface onClickListener {
+        void onItemClicked(int position);
+    }
+
+    List<String> items;
+    OnLongClickListener longClickListener;
+    onClickListener clickListener;
+
+    public itemsAdapter(List<String> items, OnLongClickListener longClickListener, onClickListener clickListener) {
+        this.items = items;
+        this.longClickListener = longClickListener;
+        this.clickListener = clickListener;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View todoView = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
+        return new ViewHolder(todoView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        String item = items.get(position);
+        holder.bind(item);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    // Container for easy access to view for each row in the list
+    class ViewHolder extends RecyclerView.ViewHolder {
+        TextView tvItem;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvItem = itemView.findViewById(android.R.id.text1);
+        }
+
+        public void bind(String item) {
+            tvItem.setText(item);
+            tvItem.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View view) {
+                    longClickListener.onItemLongClicked(getAdapterPosition());
+                    return true;
+                }
+            });
+        }
+    }
+}
